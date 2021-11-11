07b1014ca2645dc1b44be5d19e41200a43618c00
saurabh <saurabhkedar@gmail.com>
2021-11-12 01:53:34 +0530

    day1 successfully done and doccumented

--- day1-expandingcards/script.js
+++ day1-expandingcards/script.js
@@ -1,14 +1,20 @@
-const columz = document.querySelectorAll(".panel");
+const columz = document.querySelectorAll(".panel"); //selects all panels using document.qsa
 
 columz.forEach((panel) => {
+  //a foreach loop for panel
   panel.addEventListener("click", () => {
+    //above func listens to clicks and performs a function
+
+    //this belove function removes the active from all the panels explained in the function
     rmactive();
-    panel.classList.add("active");
+
+    panel.classList.add("active"); //adds a class named active
   });
 });
 
 function rmactive() {
   columz.forEach((panel) => {
-    panel.classList.remove("active");
+    // for loop that revolves through panel
+    panel.classList.remove("active"); //removes the class active
   });
 }
