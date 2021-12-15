5c0ebd33a13cd44c992791e3b5fac9a9bf7ec0d9
saurabh <saurabhkedar@gmail.com>
2021-12-15 16:54:58 +0530

    initiated functioning of Progress steps with Js

---
+++ progressSteps/script.js
@@ -0,0 +1,6 @@
+const progress = document.getElementById("progress"); // for progress bar
+const previous = document.getElementById("prev"); //for Previous
+const next = document.getElementById("next"); // for Next
+const circles = document.querySelectorAll(".circle"); // for all the number circles
+
+let currentActive = 1;
